31fc46091337602b7a112cc3f45f8d06614e2999
Ryan Thames <ryanthames@gmail.com>
2017-07-15 15:34:57 -0500

    Play around with servlets. Servlets are dum

--- src/main/java/com/example/examples/AccountServlet.java
+++ src/main/java/com/example/examples/AccountServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns="/account",
   initParams={
@@ -15,6 +16,10 @@ import java.io.IOException;
 public class AccountServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    super.doGet(req, resp);
+    try(PrintWriter out = resp.getWriter()) {
+      out.println("<html><head><title>MyServlet</title></head></html>");
+    }
+
+    String txValue = req.getParameter("tx");
   }
 }
